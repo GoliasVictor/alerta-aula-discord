572fcb3ee1f4f042b5de533c8b959331608df8d1
Yan Thomas <yanthomas.b@gmail.com>
2021-02-14 21:41:25 -0300

    Timing updates & embed styling

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const cron = require('node-cron');
 const client = new Discord.Client();
-const day = new Date().getDay();
-
 
 client.once('ready', () => {
 	console.log('Alerta Aula iniciado.');
 	const classesRawData = fs.readFileSync('classes.json');
 	const classes = JSON.parse(classesRawData);
+	const embed = new Discord.MessageEmbed();
 
-	cron.schedule('0 0 0 * * *', () => {
+	cron.schedule('0 0 1-31 * 1-5', () => {
+		const day = new Date().getDay();
 		for (const cl in classes) {
-			console.log(classes[cl][day]);
 			const todayClasses = classes[cl][day];
 			todayClasses.forEach(c => {
-				cron.schedule(c.time, () => {
+				setTimeout(() => {
 					const channel = client.channels.cache.find(channels => channels.name === c.class.toLowerCase());
-					channel.send(`A aula de ${c.name} está prestes a começar!`);
-				});
+					embed
+						.setTitle(`Aula de ${c.name}`)
+						.setColor('#0099ff')
+						.setDescription('Informações sobre a aula:')
+						.addFields(
+							{ name: 'Horário de início:', value: c.startingTime },
+							{ name: 'Horário de fim:', value: c.endingTime },
+						)
+						.setFooter('Alerta baseado no cronograma de aulas oficial');
+					channel.send(embed);
+				}, c.time);
 			});
 		}
 	});
